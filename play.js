f5d6b66550cac5e9030e86e4dc35ec107ada4c10
zahra-hm <zahra520@hotmail.com>
2020-11-05 07:39:42 +0000

    moved connect function to seprate file

--- play.js
+++ play.js
@@ -1,18 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', data => console.log(data));
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
