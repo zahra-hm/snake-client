9837a47e73505204fecd014782ceeceb38de75a3
zahra-hm <zahra520@hotmail.com>
2020-11-05 07:41:05 +0000

    connect functino from play.js file

---
+++ client.js
@@ -0,0 +1,13 @@
+const net = require('net');
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', data => console.log(data));
+  return conn;
+}
+
+module.exports = { connect }
