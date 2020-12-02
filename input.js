61543b787165337926750821e2ec7c464e31d8e7
zahra-hm <zahra520@hotmail.com>
2020-12-02 23:33:16 +0000

    set up WASD movements to print msg with key press

--- input.js
+++ input.js
@@ -10,8 +10,21 @@ const setupInput = function() {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === '\u0077') {
+    console.log('UP');
+  }
+  else if (key === '\u0073') {
+    console.log('DOWN');
+  }
+  else if (key === '\u0061') {
+    console.log('LEFT');
+  }
+  else if (key === '\u0064') {
+    console.log('RIGHT');
   }
 }
 
+// Stores the active TCP connection object.
+let connection;
 
 module.exports = { setupInput };
