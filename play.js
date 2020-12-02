5771497a2575aec525727664637f579b60e390b0
zahra-hm <zahra520@hotmail.com>
2020-12-02 23:02:31 +0000

    use stndard input to listen for keyboard input

--- play.js
+++ play.js
@@ -1,23 +1,22 @@
-// const net = require('net');
-// const { connects } = require('./client');
-// /**
-//  * Establishes connection with the game server
-//  */
-// const connect = function() {
-//   const conn = net.createConnection({ 
-//     host: 'localhost',
-//     port: 50541
-//   });
-//   // interpret incoming data as text
-//   conn.setEncoding('utf8'); 
-
-//   return conn;
-// }
-
-// console.log('Connecting ...');
-// connect();
-// // console.log('you ded cuz you didled')
 
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+setupInput();
+
