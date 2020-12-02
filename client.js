b6c9fb79441bf91bb8fbd6f46f62728a60fb1f12
zahra-hm <zahra520@hotmail.com>
2020-12-02 20:53:52 +0000

    create client and refactor play

--- client.js
+++ client.js
@@ -14,4 +14,4 @@ const connect = function() {
   return conn;
 }
 
-module.exports = { connect }
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,3 +1,23 @@
+// const net = require('net');
+// const { connects } = require('./client');
+// /**
+//  * Establishes connection with the game server
+//  */
+// const connect = function() {
+//   const conn = net.createConnection({ 
+//     host: 'localhost',
+//     port: 50541
+//   });
+//   // interpret incoming data as text
+//   conn.setEncoding('utf8'); 
+
+//   return conn;
+// }
+
+// console.log('Connecting ...');
+// connect();
+// // console.log('you ded cuz you didled')
+
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
