932a780567d2fcc580eb5fb5ed1099bdaab8d3a4
zahra-hm <zahra520@hotmail.com>
2020-12-03 00:01:06 +0000

    implement  WASD movements

--- input.js
+++ input.js
@@ -1,9 +1,14 @@
-const setupInput = function() {
+
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', handleUserInput);
+  connection = conn;
   return stdin;
 }
 
@@ -11,20 +16,17 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   } else if (key === '\u0077') {
-    console.log('UP');
+    connection.write("Move: up");
   }
   else if (key === '\u0073') {
-    console.log('DOWN');
+    connection.write("Move: down");
   }
   else if (key === '\u0061') {
-    console.log('LEFT');
+    connection.write("Move: left");
   }
   else if (key === '\u0064') {
-    console.log('RIGHT');
+    connection.write("Move: right");
   }
 }
-
-// Stores the active TCP connection object.
-let connection;
 
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 
 const { connect } = require('./client');
 const { setupInput } = require('./input');
-console.log('Connecting ...');
-connect();
-setupInput();
 
+console.log('Connecting ...');
+
+setupInput(connect());
