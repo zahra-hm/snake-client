5a95bd4bfc56c553a9ecd25157778f6f9c68e5c2
zahra-hm <zahra520@hotmail.com>
2020-12-03 00:19:31 +0000

    send message to board

--- input.js
+++ input.js
@@ -15,17 +15,25 @@ const setupInput = function(conn) {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  // key = w
   } else if (key === '\u0077') {
     connection.write("Move: up");
   }
+  // key = s
   else if (key === '\u0073') {
     connection.write("Move: down");
   }
+  // key = a
   else if (key === '\u0061') {
     connection.write("Move: left");
   }
+  // key = d
   else if (key === '\u0064') {
     connection.write("Move: right");
+  }
+  // key = h
+  else if (key === '\u0068') {
+    connection.write( "Say: Greetingssssss");
   }
 }
 
